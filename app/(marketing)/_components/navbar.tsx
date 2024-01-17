fa426540ded087db753c1498a6dc71edd8b7cc0e
tanayvaswani <vaswani.tanay9@gmail.com>
2024-01-17 15:07:40 +0530

    feat: Navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,8 +1,25 @@
+import Logo from "@frello/components/logo";
+import { Button } from "@frello/components/ui/button";
+import Link from "next/link";
+
 export const Navbar = () => {
   return (
     <div className="fixed top-0 border-b shadow-sm w-full h-14 px-4 bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center justify-between w-full">
-        navbar
+        <Logo />
+
+        <div className="flex items-center justify-between md:w-auto md:block w-full space-x-4">
+            <Button variant={"outline"} size={"sm"} asChild>
+                <Link href={"/sign-in"}>
+                    Login
+                </Link>
+            </Button>
+            <Button size={"sm"} asChild>
+                <Link href={"/sign-up"}>
+                    Try for Free!
+                </Link>
+            </Button>
+        </div>
       </div>
     </div>
   );
--- components/logo.tsx
+++ components/logo.tsx
@@ -4,14 +4,14 @@ import localFont from "next/font/local";
 import { cn } from "@frello/lib/utils";
 
 const headingFont = localFont({
-  src: "../../public/fonts/font.woff2",
+  src: "../public/fonts/font.woff2",
 });
 
 const Logo = () => {
   return (
     <Link href={"/"}>
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src={"/logo.svg"} alt={"logo"} />
+        <Image src={"/logo.svg"} alt={"logo"} height={30} width={30} />
         <p
           className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
         >
