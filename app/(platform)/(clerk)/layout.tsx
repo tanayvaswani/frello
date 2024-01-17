4355d9cbe134a2f4c6516fafc577c5ec08400f2d
tanayvaswani <vaswani.tanay9@gmail.com>
2024-01-17 16:19:54 +0530

    refac: Authentication

--- app/(platform)/(clerk)/layout.tsx
+++ app/(platform)/(clerk)/layout.tsx
@@ -1,6 +1,8 @@
 const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center bg-slate-100">{children}</div>
+    <div className="h-full flex items-center justify-center bg-slate-100">
+      {children}
+    </div>
   );
 };
 
