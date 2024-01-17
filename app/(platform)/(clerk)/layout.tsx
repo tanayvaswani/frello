72e71fda61ee936a6e379f97814f6520cb398c8d
tanayvaswani <vaswani.tanay9@gmail.com>
2024-01-17 16:10:43 +0530

    feat: Authentication

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,7 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full flex items-center justify-center bg-slate-100">{children}</div>
+  );
+};
+
+export default ClerkLayout;
